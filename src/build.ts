0e781fd22b92d63aac743be4321dffc5632657c5
thathardwareguy <ardutech11@gmail.com>
2022-06-19 21:52:19 +0100

    added ebs

--- src/build.ts
+++ src/build.ts
@@ -1,6 +1,6 @@
 import { exec } from 'child_process';
 try {
-    exec('cp package.json www/package.json && cp Procfile www/Profile && mkdir www/tmp/ && cd www && zip -r Archive.zip . && cd ..', (error, stdout, stderr) => {
+    exec('cp package.json www/package.json && mkdir www/tmp/ && cd www && zip -r Archive.zip . && cd ..', (error, stdout, stderr) => {
         console.log(error, stderr, stdout)
     });
 } catch (error) {
